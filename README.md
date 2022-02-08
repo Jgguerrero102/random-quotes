89fb2f25e1cea55512d2eccf0b6c469866e54598
Jgguerrero102 <97700013+Jgguerrero102@users.noreply.github.com>
2022-02-07 21:51:09 -0500

    February 7, 2022 9:51 PM

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # random-quotes
+
+Simple nodejs application for a class that prints random quotes. 
+
+# Favorite quotation
+
+"This is the way"
+
+-The Mandalorian
---
+++ index.js
@@ -0,0 +1,9 @@
+
+let quotes = [`"This is the way" - The Mandalorian`,
+`"Never put off till tomorrow what you can do today." - Thomas Jefferson`,
+`"Technology is the campfire around which we tell our stories." - Laurie Anderson`]
+// console.log(Math.random())
+// console.log(3 * Math.random())
+ let index = Math.floor((quotes.length * Math.random()))
+// console.log(`"This is the way" - The Mandalorian`)
+console.log(quotes[index])
