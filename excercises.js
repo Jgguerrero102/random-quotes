1f95894365cc38428c1c9ba627e9c9c79a04531d
Jgguerrero102 <97700013+Jgguerrero102@users.noreply.github.com>
2022-02-08 05:16:01 -0500

    February 8, 2022 5:16 AM

--- excercises.js
+++ excercises.js
@@ -8,9 +8,27 @@ function aFunction(){
 let functionReference = aFunction
 
 function myHigherOrderFunction(functionParameter) {
-    return functionParameter()
+    for  (let i = 0; i < n; i++) {
+    functionParameter()
+    }
 }
 
-console.log(aFunction())
-console.log(functionReference())
-console.log(myHigherOrderFunction(functionReference))
+//console.log(aFunction())
+//console.log(functionReference())
+console.log(myHigherOrderFunction(functionReference, 3))
+
+let aFunc = function() {
+    return Math.random()
+}
+aFunc()
+aFunc = undefined
+
+let arrowFunction = () => Math.random()
+console.log(arrowFunction())
+let arrowFunction2 = () => ({ a: 5 })
+console.log(arrowFunction2())
+let arrowFunction2 = () => { 
+    let ranNumber = Math.random ()
+    return { a: ranNumber }
+}
+console.log(arrowFunction())
