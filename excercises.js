1fd6eaea0b10eb22854930c77b91d83dd68c15c9
Jgguerrero102 <97700013+Jgguerrero102@users.noreply.github.com>
2022-02-08 03:10:11 -0500

    February 8, 2022 3:10 AM

--- excercises.js
+++ excercises.js
@@ -35,6 +35,9 @@ repeat(3, console.log);
 let arrays = [[1, 2, 3], [4, 5], [6]];
 // Your code here.
 // → [1, 2, 3, 4, 5, 6]
+let arrays = [[1, 2, 3], [4, 5], [6]];
+//Your code here
+// → [1, 2, 3, 4, 5, 6]
 
 
 
