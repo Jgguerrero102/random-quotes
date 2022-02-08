957a68d54e6efc5b8f44b30928a40f6054525f7e
Jgguerrero102 <97700013+Jgguerrero102@users.noreply.github.com>
2022-02-08 05:46:20 -0500

    February 8, 2022 5:46 AM

--- excercises.js
+++ excercises.js
@@ -55,3 +55,6 @@ export function flattening(list){
 }
 // → [1, 2, 3, 4, 5, 6]
 console.log(flattening(arrays))
+
+// 3 - 2 loop(value, test, update, body)
+//Write a higher-order function loop that provides something like a for loop statement. It takes a value, a test function, an update function, and a body function. Each iteration, it first runs the test function on the current loop value and stops if that returns false. Then it calls the body function, giving it the current value. Finally, it calls the update function to create a new value and starts from the beginning.
