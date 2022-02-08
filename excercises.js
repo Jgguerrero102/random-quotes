968658a05e502c12ab5e333cea728ab77262fffb
Jgguerrero102 <97700013+Jgguerrero102@users.noreply.github.com>
2022-02-07 22:37:19 -0500

    February 7, 2022 10:37 PM

---
+++ excercises.js
@@ -0,0 +1,12 @@
+// What is a function in Javascript
+
+function myFunction() {
+    return 1;
+}
+
+let myFunc = function myFunction() {
+    return 1;
+}
+
+console.log(myFunction())
+console.log(myFunc())
